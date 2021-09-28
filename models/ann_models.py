8771163a5c8608a1d12cca7b9c4670f823592ab1
Kaustav Ghosh <teetangh@gmail.com>
2021-09-28 11:01:14 +0530

    added ANN2 model

--- models/ann_models.py
+++ models/ann_models.py
@@ -3,7 +3,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.python import keras
 
 
-class CustomANNModel():
+class ANN1():
 
     def __init__(self) -> None:
         self.dense1 = Dense(input_dim=784, units=512, activation="relu",
@@ -17,14 +17,6 @@ class CustomANNModel():
         self.dense5 = Dense(units=1, activation="sigmoid",
                             kernel_initializer="glorot_uniform", bias_initializer="zeros")
 
-    def forward(self, x):
-        x = self.dense1(x)
-        x = self.dense2(x)
-        x = self.dense3(x)
-        x = self.dense4(x)
-        x = self.dense5(x)
-        return x
-
     def train(self, X_train, X_test, y_train, y_test, epochs=10, batch_size=32):
         model = Sequential()
         model.add(self.dense1)
@@ -34,7 +26,34 @@ class CustomANNModel():
         model.add(self.dense5)
         model.compile(loss="categorical_crossentropy",
                       optimizer="adam", metrics=["accuracy"])
-        model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size,verbose=1)
+        model.fit(X_train, y_train, epochs=epochs,
+                  batch_size=batch_size, verbose=1)
+        y_pred = model.predict(X_test)
+        print(y_pred)
+
+        print("Evaluating on test data")
+        results = model.evaluate(X_test, y_test, batch_size=batch_size)
+        print("test loss, test acc:", results)
+
+
+class ANN2():
+    def __init__(self) -> None:
+        self.dense1 = Dense(input_dim=784, units=800, activation="relu",
+                            kernel_initializer="glorot_uniform", bias_initializer="zeros")
+        self.dense2 = Dense(units=10, activation="softmax",
+                            kernel_initializer="glorot_uniform", bias_initializer="zeros")
+        self.dense3 = Dense(units=1, activation="sigmoid",
+                            kernel_initializer="glorot_uniform", bias_initializer="zeros")
+
+    def train(self, X_train, X_test, y_train, y_test, epochs=10, batch_size=32):
+        model = Sequential()
+        model.add(self.dense1)
+        model.add(self.dense2)
+        model.add(self.dense3)
+        model.compile(loss="categorical_crossentropy",
+                      optimizer="adam", metrics=["accuracy"])
+        model.fit(X_train, y_train, epochs=epochs,
+                  batch_size=batch_size, verbose=1)
         y_pred = model.predict(X_test)
         print(y_pred)
 
--- src/run_ann.py
+++ src/run_ann.py
@@ -3,7 +3,7 @@ import os
 
 from scipy.sparse.construct import rand
 from tensorflow.python.keras.backend import shape
-from models.ann_models import CustomANNModel
+from models.ann_models import ANN1, ANN2
 import matplotlib.pyplot as plt
 import sys
 import numpy as np
@@ -13,15 +13,15 @@ from sklearn.model_selection import train_test_split
 
 
 def main():
-    model = CustomANNModel()
     data = pd.read_csv(os.path.join(
         os.path.dirname(__file__), "../data/train.csv"))
-
     X, y = data.iloc[:, 1:].values, data.iloc[:, 0].values
-    print(X.shape, y.shape)
+    print("The shape of the input is: ", X.shape, y.shape)
     X_train, X_valid, y_train, y_test = train_test_split(
         X, y, test_size=0.2, random_state=42)
 
+    # model = ANN1()
+    model = ANN2()
     model.train(X_train, X_valid, y_train, y_test, epochs=3, batch_size=32)
 
 
