4c6a7b9bd8b04d2134660718b27c286e51741f32
Kaustav Ghosh <teetangh@gmail.com>
2021-09-29 10:55:44 +0530

    added preprocessing of images

--- models/cnn_models.py
+++ models/cnn_models.py
@@ -1,4 +1,7 @@
-from tensorflow.keras.layers import Conv2D, MaxPool2D, Dropout, ReLU
+from warnings import filters
+from tensorflow.keras.layers import Conv2D, MaxPool2D, Dropout, Dense
+from tensorflow.keras.models import Sequential
+from tensorflow.python.keras.layers.core import Flatten
 
 
 class CNNDefaultModel():
@@ -6,12 +9,46 @@ class CNNDefaultModel():
     def __init__(self) -> None:
         pass
 
+    def train(self, X_train, X_valid, y_train, y_valid,
+              epochs=10, batch_size=32,
+              loss="sparse_categorical_crossentropy",
+              optimizer="adam", metrics=["accuracy"]
+              ):
+        self.model.compile(loss=loss,
+                           optimizer=optimizer, metrics=metrics)
+        self.model.fit(X_train, y_train, epochs=epochs,
+                       batch_size=batch_size, verbose=1)
+        y_pred = self.model.predict_classes(X_valid)
+        print(y_pred)
+
+        print("Evaluating on valid data")
+        results = self.model.evaluate(X_valid, y_valid, batch_size=batch_size)
+        print("valid loss, valid acc:", results)
+
+    def predict_test_classes(self, X_test):
+        y_pred = self.model.predict_classes(X_test)
+        return y_pred
+
 
 class CNN1(CNNDefaultModel):
 
     def __init__(self) -> None:
         super(CNNDefaultModel, self).__init__()
-        
-        self.conv2d1 = Conv2D() 
 
-        
+        self.conv2d1 = Conv2D(input_shape=(28, 28, 1), filters=64, kernel_size=(
+            1, 1), activation="relu", padding="valid")
+        self.conv2d2 = Conv2D(filters=32, kernel_size=(
+            1, 1), activation="relu", padding="valid")
+        self.pool2d1 = MaxPool2D()
+        self.flatten1 = Flatten()
+        self.dropout1 = Dropout(0.2)
+        self.fc1 = Dense(units=10, activation="softmax",
+                         kernel_initializer="he_uniform", bias_initializer="zeros")
+
+        self.model = Sequential()
+        self.model.add(self.conv2d1)
+        self.model.add(self.conv2d2)
+        self.model.add(self.pool2d1)
+        self.model.add(self.flatten1)
+        self.model.add(self.dropout1)
+        self.model.add(self.fc1)
--- src/run_cnn.py
+++ src/run_cnn.py
@@ -1,11 +1,35 @@
+import warnings
 import csv
 import os
 
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 from models.cnn_models import CNN1
 from sklearn.model_selection import train_test_split
+
+from tensorflow.keras.utils import to_categorical
+
+
+def plot_image_grid(X, y, nrows, ncols):
+    fig, axes = plt.subplots(nrows=nrows, ncols=ncols)
+
+    for i in range(nrows * ncols):
+        # axes[row, col] = plt.imshow(X_train[count])
+        plt.subplot(nrows, ncols, i + 1)
+        plt.imshow(X[i])
+        plt.xlabel(np.argmax(y[i]))
+    plt.show()
+
+
+def preprocess_image_input(X):
+    X = X.reshape(X.shape[0], 28, 28, 1)
+    X = X.astype('float32')
+
+    # RGB values are usually stored as integers to save memory. But doing math on colors is usually done in float because it's easier, more powerful, and more precise. The act of converting floats to integers is called "Quantization", and it throws away precision.
+
+    # Typically, RGB values are encoded as 8-bit integers, which range from 0 to 255. It's an industry standard to think of 0.0f as black and 1.0f as white (max brightness). To convert [0, 255] to [0.0f, 1.0f] all you have to do is divide by 255.0f.
+    X /= 255
 
 
 def main():
@@ -15,15 +39,20 @@ def main():
         os.path.dirname(__file__), "../data/test.csv"))
 
     X, y = df_train.iloc[:, 1:].values, df_train.iloc[:, 0].values
-    X = X.reshape(X.shape[0], 28, 28)
-    X = X.astype('float32')
-    # X /= 255
+    X_test = df_test.iloc[0:, 0:].values
+
+    preprocess_image_input(X)
+    preprocess_image_input(X_test)
+
     y = np.array(y)
     y = y.reshape(y.shape[0], 1)
     y = y.astype('int32')
 
-    X_train, X_valid, y_train, y_test = train_test_split(
+    X_train, X_valid, y_train, y_valid = train_test_split(
         X, y, test_size=0.1, random_state=42)
+
+    y_train = to_categorical(y_train)
+    y_valid = to_categorical(y_valid)
 
     print(X_train[0].shape)
     # print(X[0:5], y[0:5])
@@ -32,14 +61,12 @@ def main():
 
     nrows = 2
     ncols = 3
-    fig, axes = plt.subplots(nrows=nrows, ncols=ncols)
 
-    for i in range(nrows * ncols):
-        # axes[row, col] = plt.imshow(X_train[count])
-        plt.subplot(nrows, ncols, i + 1)
-        plt.imshow(X_train[i])
-        plt.xlabel(y_train[i])
-    plt.show()
+    warnings.simplefilter(action='ignore', category=FutureWarning)
+    # plot_image_grid(X_train, y_train, nrows, ncols)
+
+    model = CNN1()
+    model.train()
 
 
 if __name__ == '__main__':
